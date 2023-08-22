a6d55c4d64b83003aabe2279aaa303afe1af2e72
worathepnot <worathepboonsak@gmail.com>
2023-08-23 00:37:09 +0700

    commit

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: const [
           Color.fromARGB(255, 26, 2, 89),
           Color.fromARGB(255, 45, 7, 98)
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ], begin: startAlignment, end:endAlignment),
       ),
-      child: const Center(
-        child: Text(
-          'hello',
-          style: TextStyle(color: Colors.greenAccent, fontSize: 50),
-        ),
-      ),
+      child: const Center(child: StyledText()),
     );
   }
 }
---
+++ lib/styled_text.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText({super.key}); 
+  @override
+  Widget build(context) {
+    return const Text(
+          'hello',
+          style: TextStyle(color: Colors.white, 
+          fontSize: 28,
+          ),
+    );
+  }
+}
